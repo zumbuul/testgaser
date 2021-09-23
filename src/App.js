bde4fcf73e66ffd09f594decd2c646af481eea52
zumbuul <melje1delje@gmail.com>
2021-09-23 19:09:18 +0200

    added nenad component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nenad from "./Nenad.jsx";
 
 function App() {
   return (
@@ -7,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          IDE GAS
+          Edit <code>src/App.js</code> and save to reload. IDE GAS
         </p>
         <a
           className="App-link"
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Nenad></Nenad>
       </header>
     </div>
   );
---
+++ src/Nenad.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Nenad = () => {
+  return <div>NENAD !!!</div>;
+};
+
+export default Nenad;
